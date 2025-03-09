5a4e84b84e0f530ba31257c4b8e78076643f88ab
gusperales06 <121507623+gusperales06@users.noreply.github.com>
2025-03-08 18:03:10 -0600

    Initial commit

---
+++ src/main/java/com/customerownership/CustomerOwnershipCarCompanyApiApplication.java
@@ -0,0 +1,13 @@
+package com.customerownership;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerOwnershipCarCompanyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerOwnershipCarCompanyApiApplication.class, args);
+	}
+
+}
